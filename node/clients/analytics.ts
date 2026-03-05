64e784d3348eeacdba6dbe2db6802e6a88b3a534
BlancatoAntonino <antoninoblancat01@gmail.com>
2026-03-05 15:09:12 +0100

    Funzionano le chiamate postman

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,3 +1,22 @@
-import { AppClient } from '@vtex/api'
+//node/clients/analytics.ts
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
 
-export default class Analytics extends AppClient {}
+export default class Analytics extends AppClient {
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super('replyqa1.mocked-analytics@0.x', context, options)
+  }
+
+  public getLiveUsers(): Promise<LiveUsersProduct[]> {
+    return this.http.get('_v/live-products')
+  }
+}
+
+// Promise<LiveUsersProduct[]> è ciò che il metodo deve tornare: nel nostro
+//caso la Promise di un Array di oggetti di tipo LiveUserProduct -> lo definisco nell'interfaccia (più giù)
+//L'endpoint GET _v/live-producs appartiene al servizio mocked-analytics
+
+//Definisco l'oggetto LiveUserProduct
+interface LiveUsersProduct {
+  slug: string
+  liveUsers: number
+}
